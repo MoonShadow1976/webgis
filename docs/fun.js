a47e14d7fd0f8cdcf751393dc89c26ecaa4657e8
MoonShadow1976 <MoonShadow1976@outlook.com>
2024-03-28 16:58:41 +0800

    add-polyline-fun

--- docs/fun.js
+++ docs/fun.js
@@ -13,13 +13,42 @@ function Action(json) {
           console.log("点", lnglats);
         },
         getPolyline: function (geojson, lnglats) {
-          console.log("线", lnglats);
+          console.log("线", lnglats)
+          const Pol = onCreatePolyline(lnglats)
+          return Pol // 一定要返回实例 不能删
         }
       })
       map.setFitView(null, false, [120, 2, 2, 2], 14)
     })
   }
   function onCreatePolygon(lnglats) {
+    const styleRow = {
+      strokeColor: '#00E1FF',
+      strokeOpacity: 0.5,
+      strokeWeight: 2,
+      fillColor: '#003666',
+      fillOpacity: 0.5
+    }
+    const POLY = new AMap.Polygon({
+      path: lnglats,
+      bubble: true,
+      cursor: 'pointer',
+      map: map,
+      ...styleRow
+    })
+    POLY.on('mouseover', () => {
+      POLY.setOptions({
+        strokeColor: '#0092bc',
+        strokeOpacity: 1,
+        strokeWeight: 3,
+        fillColor: '#00427d',
+        fillOpacity: 0.6
+      })
+    })
+    POLY.on('mouseout', () => POLY.setOptions(styleRow))
+    return POLY
+  }
+  function onCreatePolyline(lnglats) {
     const styleRow = {
       strokeColor: '#00E1FF',
       strokeOpacity: 0.5,
